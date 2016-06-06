6690389d2e5a82d60909bf758c28f3f5d285e896
Rodrigo Braz Monteiro <archmagezeratul@gmail.com>
2016-06-06 20:04:12 +0100

    Fix dynamic collision checking.

--- samples/trapped/src/systems/check_dynamic_collision_system.cpp
+++ samples/trapped/src/systems/check_dynamic_collision_system.cpp
@@ -4,19 +4,20 @@ class CheckDynamicCollisionSystem final : public CheckDynamicCollisionSystemBase
 public:
 	void update(Halley::Time time, MainFamily& e) {
 		// TODO: make this more efficient
+
+		// Skip static objects
 		if (e.collider->isStatic) {
 			return;
 		}
 
 		auto myRect = e.position->position + e.collider->rect;
 		for (auto& o : targetsFamily) {
-			if (o.collider->isStatic) {
-				return;
-			}
-
-			auto rect = o.position->position + o.collider->rect;
-			if (myRect.overlaps(rect)) {
-				sendMessage(e.entityId, CollisionMessage(o.entityId, o.collider->layer));
+			// Don't check against static objects, or against myself
+			if (!o.collider->isStatic && o.entityId != e.entityId) {
+				auto rect = o.position->position + o.collider->rect;
+				if (myRect.overlaps(rect)) {
+					sendMessage(e.entityId, CollisionMessage(o.entityId, o.collider->layer));
+				}
 			}
 		}
 	}
